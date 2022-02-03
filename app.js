ac0d9865285fcb550ec2136976f09a4b3231aa3d
James McNeil <mcneil_james@hotmail.com>
2022-02-03 20:11:46 +0000

    added handleClick empty function and looped keys array to create button with key value

--- app.js
+++ app.js
@@ -2,36 +2,43 @@ const tileDisplay = document.querySelector('.tile-container')
 const keyboard = document.querySelector('.key-container')
 
 const keys = [
-	'Q',
-	'W',
-	'E',
-	'R',
-	'T',
-	'Y',
-	'U',
-	'I',
-	'O',
-	'P',
-	'A',
-	'S',
-	'D',
-	'F',
-	'G',
-	'H',
-	'J',
-	'K',
-	'L',
-	'ENTER',
-	'Z',
-	'X',
-	'C',
-	'V',
-	'B',
-	'N',
-	'M',
-	'<=',
+  'Q',
+  'W',
+  'E',
+  'R',
+  'T',
+  'Y',
+  'U',
+  'I',
+  'O',
+  'P',
+  'A',
+  'S',
+  'D',
+  'F',
+  'G',
+  'H',
+  'J',
+  'K',
+  'L',
+  'ENTER',
+  'Z',
+  'X',
+  'C',
+  'V',
+  'B',
+  'N',
+  'M',
+  '<=',
 ]
+const handleClick = () => {
+  console.log('clicked')
+}
 
 keys.forEach((key) => {
-	document.createElement('button')
+  const buttonElement = document.createElement('button')
+  buttonElement.textContent = key
+  buttonElement.setAttribute('id', key)
+  buttonElement.addEventListener('click', handleClick)
+  keyboard.append(buttonElement)
 })
